c9b7d84387ddb2e90a1ceb14c67bb2b57586ce93
eliezerantonio <eliezerfernandoantonio@gmail.com>
2021-08-05 01:42:18 +0100

    refactor: add customized error for missing param

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,12 +1,12 @@
 import { SignUpController } from './signup'
-
+import { MissingParamError } from '../errors/missing-param-error'
 describe('SignUp Controller', () => {
   test('should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     const httpRequest = { body: { email: 'any_email@gmail.com', password: 'any_password', passwordConfirmation: 'any_password' } }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
   test('should return 400 if no email is provided', () => {
@@ -14,6 +14,6 @@ describe('SignUp Controller', () => {
     const httpRequest = { body: { name: 'any_name', password: 'any_password', passwordConfirmation: 'any_password' } }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 })
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,11 +1,13 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-error'
+
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return { statusCode: 400, body: new Error('Missing param: name') }
+      return { statusCode: 400, body: new MissingParamError('name') }
     }
     if (!httpRequest.body.email) {
-      return { statusCode: 400, body: new Error('Missing param: email') }
+      return { statusCode: 400, body: new MissingParamError('email') }
     }
   }
 }
